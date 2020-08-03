d873b69f3e6969d37134aa2e60e9de3b470d8b56
cherry99330 <cherrystuti1999@gmail.com>
2020-08-03 15:48:13 +0530

    scaling animation added

---
+++ sih2020_IMAR/Assets/start.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public void start () 
+	{
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+		
+
+}
+
